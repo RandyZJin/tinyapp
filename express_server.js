19f0e17e994b41df49ede6d6cd784258a76b6d0e
RJ <randy.j416@yahoo.com>
2023-01-23 15:51:14 -0500

    added shortening and redirecting functions

--- express_server.js
+++ express_server.js
@@ -5,10 +5,6 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
-});
 
 function generateRandomString() {
   return Math.random().toString(36).slice(7)
@@ -19,6 +15,15 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  let newID = generateRandomString();
+  console.log(newID);
+  urlDatabase[newID] = req.body.longURL;
+  // res.send("http://www.google.com");
+  res.redirect(`/urls/${newID}`); 
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -34,8 +39,15 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
+  // console.log(urlDatabase[req.params.id])
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:id", (req, res) => {
+
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 });
 
 app.get("/urls.json", (req, res) => {
